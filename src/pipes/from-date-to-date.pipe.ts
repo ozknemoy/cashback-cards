504e9886e2603ececb2fcf3c464d9bd1f3665afb
ozknemoy <ozk.nemoy@mail.ru>
2018-10-05 18:58:55 +0300

    fix date filter for safari

--- src/pipes/from-date-to-date.pipe.ts
+++ src/pipes/from-date-to-date.pipe.ts
@@ -3,18 +3,31 @@
  */
 import { Pipe } from '@angular/core';
 
-@Pipe({name: 'fromDateToDate'})
-export class FromDateToDatePipe  {
-    transform(array:any[], st: string, en: string, key = 'date'):any[] {
-      // todo isWithinRange 'date-fns'
-      if (!array) return undefined;
-      if(!st&&!en) return array;
-      const start = st? +new Date(st):0;
-      const end = en? +new Date(en):+new Date('2222-11-11');
+function __parseDate(input, format = 'yyyy-mm-dd') {
+  const parts = input.match(/(\d+)/g);
+  let i = 0;
+  const fmt = {};
+  // extract date-part indexes from the format
+  format.replace(/(yyyy|dd|mm)/g, (part) => fmt[part] = <any>(i++));
+return new Date(parts[fmt['yyyy']], parts[fmt['mm']] - 1, parts[fmt['dd']]);
+}
 
-      return array.filter(item => {
-        let med = +new Date(item[key]);
-        return med>=start && med<=end
-      })
+@Pipe({name: 'fromDateToDate'})
+export class FromDateToDatePipe {
+  transform(array: any[], st: string, en: string, key = 'date'): any[] {
+    // todo isWithinRange 'date-fns'
+    if (!array) {
+      return undefined;
     }
+    if (!st && !en) {
+      return array;
+    }
+    const start = st ? +new Date(st) : 0;
+    const end = en ? +new Date(en) : +new Date('2222-11-11');
+
+    return array.filter(item => {
+      const med = +__parseDate(item[key]);
+      return med >= start && med <= end;
+    });
+  }
 }
